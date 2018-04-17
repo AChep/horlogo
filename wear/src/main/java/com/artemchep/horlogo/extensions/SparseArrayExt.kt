3ae0d1d0681be5119ad68ec8a90313a6608a2e02
Artem Chepurnoy <mail@artemchep.com>
2018-04-17 10:27:08 +0300

    SparseArrayExt: Crossinline the 'block' in foreach

--- wear/src/main/java/com/artemchep/horlogo/extensions/SparseArrayExt.kt
+++ wear/src/main/java/com/artemchep/horlogo/extensions/SparseArrayExt.kt
@@ -2,7 +2,7 @@ package com.artemchep.horlogo.extensions
 
 import android.util.SparseArray
 
-inline fun <T> SparseArray<T>.forEachIndexed(block: (index : Int, value: T) -> Unit) {
+inline fun <T> SparseArray<T>.forEachIndexed(crossinline block: (index : Int, value: T) -> Unit) {
     val size = this.size()
     for (i in 0 until size) {
         val value = this.valueAt(i)
