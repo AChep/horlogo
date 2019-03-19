2f2be23342ecd80f34de78e2480ef790a443bdd6
Artem Chepurnoy <artemchep@gmail.com>
2019-03-19 18:49:25 +0200

    Make BrandTextView extend AppCompatTextView

--- common/src/main/java/com/artemchep/horlogo/ui/views/BrandTextView.kt
+++ common/src/main/java/com/artemchep/horlogo/ui/views/BrandTextView.kt
@@ -2,7 +2,7 @@ package com.artemchep.horlogo.ui.views
 
 import android.content.Context
 import android.util.AttributeSet
-import android.widget.TextView
+import androidx.appcompat.widget.AppCompatTextView
 import com.artemchep.horlogo.ui.Fonts
 
 /**
@@ -12,7 +12,7 @@ class BrandTextView @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
-) : TextView(context, attrs, defStyleAttr) {
+) : AppCompatTextView(context, attrs, defStyleAttr) {
 
     init {
         typeface = Fonts.getTypeface(context.assets, Fonts.OVERLOCK_REGULAR)
